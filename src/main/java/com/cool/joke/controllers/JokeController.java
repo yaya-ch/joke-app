bfd241f9456983d5685e8dfb206750920c896bc8
yaya <cherifi.yahia@yahoo.fr>
2020-05-30 02:06:18 +0200

    joke controller created

--- src/main/java/com/cool/joke/controllers/JokeController.java
+++ src/main/java/com/cool/joke/controllers/JokeController.java
@@ -1,4 +1,30 @@
 package com.cool.joke.controllers;
 
+import com.cool.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class JokeController {
+
+    private JokeService jokeService;
+
+    /**
+     * the Autowired annotation is optional
+     * but i put it there for clarifications
+     * @param jokeService
+     */
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
 }
